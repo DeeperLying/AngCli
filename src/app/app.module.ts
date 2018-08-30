6d4a94fb4ed59b9677da36ccd85073dc3a867521
Deeper-Lee <984130957@qq.com>
2018-08-30 16:09:33 +0800

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+<<<<<<< HEAD
 import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchComponent
+=======
+
+@NgModule({
+  declarations: [
+    AppComponent
+>>>>>>> parent of 192ed52... 首页header
   ],
   imports: [
     BrowserModule
