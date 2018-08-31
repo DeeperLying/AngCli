e554fcd23e133f5e8b4e35ce986fe65d98543fc0
Deeper-Lee <984130957@qq.com>
2018-08-31 14:09:59 +0800

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { BmMatchComponent } from './bm-match/bm-match.component';
 import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'search', pathMatch: 'full' },
-  { path: 'matches', component: AppComponent },
+  { path: '', redirectTo: 'matches', pathMatch: 'full' },
+  { path: 'matches', component: BmMatchComponent },
   { path: 'search', component: SearchComponent }
 ];
 
