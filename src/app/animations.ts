992282e67faabeb8e2ffd5f80a00b74aeb8c1833
Deeper-Lee <984130957@qq.com>
2018-08-31 15:01:10 +0800

    路由动画修改

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -6,20 +6,20 @@ export const slideInDownAnimation =
     state('*',
       style({
         opacity: 1,
-        transform: 'translateX(0)'
+        transform: 'translateX(0)',
+        width: 100 + '%',
       })
     ),
     transition(':enter', [ // 入场动画
       style({
-        opacity: 0,
         transform: 'translateX(100%)'
       }),
-      animate('0.2s ease-in')
+      animate('0.5s ease')
     ]),
     transition(':leave', [ // 出场动画
-      animate('0.2s ease-out', style({
+      animate('0.3s ease', style({
         opacity: 0,
-        transform: 'translateY(-30%)'
+        transform: 'translateX(0%)'
       }))
     ])
   ] );
--- src/app/bm-match/bm-match.component.ts
+++ src/app/bm-match/bm-match.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostBinding, OnInit} from '@angular/core';
+
+import { slideInDownAnimation } from '../animations';
 
 @Component({
   selector: 'app-bm-match',
   templateUrl: './bm-match.component.html',
-  styleUrls: ['./bm-match.component.css']
+  styleUrls: ['./bm-match.component.css'],
+  animations: [ slideInDownAnimation ]
 })
 export class BmMatchComponent implements OnInit {
-
+  @HostBinding('@routeAnimation') routeAnimation = true;
+  @HostBinding('style.display')   display = 'block';
+  @HostBinding('style.position')  position = 'absolute';
   constructor() { }
 
   ngOnInit() {
