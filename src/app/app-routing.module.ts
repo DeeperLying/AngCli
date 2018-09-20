3e23135a46e9ab377470123a4a9dda0630e43f87
Deeper-Lee <984130957@qq.com>
2018-09-20 09:29:07 +0800

    赛事详情页面

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { BmMatchComponent } from './bm-match/bm-match.component';
 import { SearchComponent } from './search/search.component';
+import { BmMatchDetailComponent } from './bm-match-detail/bm-match-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'matches', pathMatch: 'full' },
   { path: 'matches', component: BmMatchComponent },
-  { path: 'search', component: SearchComponent }
+  { path: 'search', component: SearchComponent },
+  { path: 'matcheDetail/:id', component: BmMatchDetailComponent }
 ];
 
 @NgModule({
--- src/app/bm-match-detail/bm-match-detail.component.ts
+++ src/app/bm-match-detail/bm-match-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-bm-match-detail',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bm-match-detail.component.css']
 })
 export class BmMatchDetailComponent implements OnInit {
-
-  constructor() { }
+  private localhost: Location;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    this.route.paramMap.pipe(
+      (param) => {
+        console.log(param);
+        return param;
+      },
+    );
+  }
+
+  back (): void {
+    window.history.back();
   }
 
 }
