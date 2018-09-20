954530e22078a3fea6c7a813403116e0ed04e6f8
Deeper-Lee <984130957@qq.com>
2018-09-20 11:35:29 +0800

    赛事内页做完，马上开始做路由守卫，路由守卫做完就做登录注册

--- src/app/bm-match-detail/bm-match-detail.component.ts
+++ src/app/bm-match-detail/bm-match-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router'; // 路由监控
 import { Location } from '@angular/common';
+import { BmbService } from '../bmb.service';
+
 
 @Component({
   selector: 'app-bm-match-detail',
@@ -9,22 +11,39 @@ import { Location } from '@angular/common';
 })
 export class BmMatchDetailComponent implements OnInit {
   private localhost: Location;
+  private matchId: string;
+  public matchs: object[];
+  public winWidth: number;
   constructor(
     private route: ActivatedRoute,
     private router: Router,
+    private server: BmbService
   ) { }
 
   ngOnInit() {
-    this.route.paramMap.pipe(
-      (param) => {
-        console.log(param);
-        return param;
-      },
-    );
+    this.matchId = this.route.snapshot.paramMap.get('id');
+    this.winWidth = document.body.clientWidth;
+    this.getBMMatchDetail();
   }
 
   back (): void {
     window.history.back();
+  }
+
+  private getBMMatchDetail () {
+    const param = {
+      matchId: this.matchId,
+    };
+    this.server.getDatas('GetBMMatchDetail', param ).subscribe(
+      ( res ) => {
+        this.matchs = res.messages.data;
+        console.log(this.matchs);
+      },
+      error => {
+        console.log(error);
+      },
+      () => {}
+    );
   }
 
 }
